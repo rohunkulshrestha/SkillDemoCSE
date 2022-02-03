f29241aff15435d844174212d12b11cb30a562b2
rohunkulshrestha <rohunk03@gmail.com>
2022-02-03 03:03:34 -0600

    setup

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,7 @@
-
+public class SkillDemo
+{
+    public static int multiply(int x, int y)
+    {
+        return x*y;
+    }
+}
--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -1,1 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.*;
 
+public class SkillDemoTest{
+
+    @Test
+    public void testMultiply()
+    {
+        assertEquals("Check whether it multiples correctly", 6, SkillDemo.multiply(2, 2));
+    }
+
+}
